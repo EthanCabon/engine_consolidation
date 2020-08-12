47b931060e0ff613ad6535bfc7ab558361235afb
EthanCabon <ethan.cabon@grenoble-inp.org>
2020-08-12 15:40:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ deck = Deck(cwd + "/deck_printing3D.yaml")
 geometry = Geometry_Printing3D(deck)
 
 meshing = Meshing_Layer(deck, geometry)
+
+boundary_conditions = Boundary_Conditions(meshing)
+
+heat_transfer = Heat_Transfer(meshing)
+
+temperature_change = Temperature_Change(deck, meshing, boundary_conditions, heat_transfer)
